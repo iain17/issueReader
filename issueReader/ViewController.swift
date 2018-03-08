0eca48338c543d1006403b91a97d835f5e0ab149
Iain Munro <iain17@me.com>
2018-03-08 21:37:55 +0100

    Initial Commit

---
+++ issueReader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  issueReader
+//
+//  Created by Iain Munro on 08/03/2018.
+//  Copyright © 2018 Iain Munro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
